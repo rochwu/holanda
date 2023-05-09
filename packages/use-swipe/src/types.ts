6b4759eaf50d29ed863cce63acdeb442933d4b19
Rolando Wu <rochwu@users.noreply.github.com>
2023-05-08 22:37:59 -0700

    Swipe + Vite (#18)

---
+++ packages/use-swipe/src/types.ts
@@ -0,0 +1,6 @@
+export declare namespace Swipe {
+  type Touch = 'pressed' | 'released';
+  type Direction = 'up' | 'right' | 'down' | 'left';
+  type Action = Touch | Direction;
+  type Handler = (action: Action) => void;
+}
