1b3d9d20209eca40eef7675728b31cf535f8108a
Rolando Wu <rolando.che.wu@gmail.com>
2023-05-09 00:25:44 -0700

    Made tips more efficient

--- src/Receipt/Tips/CustomTips.tsx
+++ src/Receipt/Tips/CustomTips.tsx
@@ -10,39 +10,30 @@ import {Base} from './Base';
 type Props = {};
 
 export const CustomTips: FC<Props> = () => {
-  const numberId = useId();
+  const id = useId();
   const percentId = useId();
 
   const total = useValue(Ids.LineTotal);
   const percent = useValue(percentId);
   const selectTips = useStore(
-    useCallback((state) => state.selectTips(numberId), [numberId]),
+    useCallback((state) => state.selectTips(id), [id]),
   );
-  const selectId = useStore(
-    useCallback((state) => state.select(percentId), [percentId]),
-  );
-  const setValue = useStore(
-    useCallback((state) => state.setValue(numberId), [numberId]),
-  );
+
+  const setValue = useStore(useCallback((state) => state.setValue(id), [id]));
 
   useEffect(() => {
     setValue((percent / 100) * total);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [total, percent]);
 
-  const select = () => {
-    selectId();
-    selectTips();
-  };
-
   return (
     <Line>
       <Field
         identifier={percentId}
-        onClick={select}
+        onClick={selectTips}
         width={spacing.shortInput}
       />
-      <Base identifier={numberId} select={select} percent={percent} />
+      <Base identifier={id} percent={percent} />
     </Line>
   );
 };
--- src/Receipt/Tips/Tips.tsx
+++ src/Receipt/Tips/Tips.tsx
@@ -1,6 +1,5 @@
-import {FC, useCallback, useId} from 'react';
+import {FC, useId} from 'react';
 
-import {useStore} from '../../store';
 import {Line} from '../Line';
 
 import {Base} from './Base';
@@ -13,13 +12,11 @@ type Props = {
 export const Tips: FC<Props> = ({percent, heading}) => {
   const id = useId();
 
-  const select = useStore(useCallback((state) => state.selectTips(id), [id]));
-
   const label = `${percent}%`;
 
   return (
     <Line divide={!!heading} heading={heading} label={label}>
-      <Base identifier={id} select={select} percent={percent} readOnly />
+      <Base identifier={id} percent={percent} />
     </Line>
   );
 };
